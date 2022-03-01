b581aca987a7b1a5b393eab70a1867e2cf22a016
javascriptkids <rizkycode@protonmail.com>
2022-03-01 15:27:20 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  return res.status(200).send("<h1>Hello World!</h1>");
+});
+
+app.listen(PORT, () => console.log("[!] Listening on port " + PORT));
