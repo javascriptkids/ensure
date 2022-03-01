a880ef2119b893d1d097e4a9304ba71651744269
javascriptkids <rizkycode@protonmail.com>
2022-03-01 15:36:33 +0700

    World war

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  return res.status(200).send("<h1>Hello World!</h1>");
+  return res.status(200).send("<h1>Hello World War!</h1>");
 });
 
 app.listen(PORT, () => console.log("[!] Listening on port " + PORT));
